5d36bd6ee2ccc1a0aba4004fde381d42363172cf
Homero Graco <homero.graco@gmail.com>
2016-04-21 10:35:12 +0200

    Finished CRUD for questions.

--- main.js
+++ main.js
@@ -11,12 +11,12 @@ app.config(function ($stateProvider, $urlRouterProvider) {
             .state('classificator', {
                 url: "/",
                 templateUrl: 'module/classificator/classificator.html',
-                controller: 'questions_controller'
+                controller: 'whatever_else_questions_controller'
             })
             .state('questions', {
                 url: "/questions",
                 templateUrl: 'module/config/questions/questions.html',
-                controller: 'questions_controller'
+                controller: 'edit_questions_controller'
             })
             .state('answers', {
                 url: "/answers",
@@ -26,12 +26,12 @@ app.config(function ($stateProvider, $urlRouterProvider) {
             .state('edit_question', {
                 url: "/edit_question/:question_id",
                 templateUrl: 'module/config/questions/form.html',
-                controller: 'questions_controller'
+                controller: 'edit_questions_controller'
             })
             .state('create_question', {
                 url: "/create_question",
                 templateUrl: 'module/config/questions/form.html',
-                controller: 'questions_controller'
+                controller: 'create_questions_controller'
             });
 
     $urlRouterProvider.otherwise('/');
@@ -39,25 +39,24 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 
 // APP CONTROLLERS
 
-app.controller('myController', function (myService, $scope) {
-    $scope.myData = myService;
-    $scope.myData.loadItems();
-
-    $scope.save = function () {
-        $scope.myData.saveItem();
+app.controller('create_questions_controller', function(question_service, $stateParams, $state, $scope) {
+    
+    $scope.mode = "create";
+    
+    $scope.question_data = question_service;
+    $scope.question_data.current_item = null;
+    
+    $scope.create_question = function () {
+        $scope.question_data.create_items($scope.question_data.current_item);
+        $scope.question_data.load_items();
+        $scope.question_data.current_item = null;
+        $state.go('questions');
     };
-
-    $scope.clear = function () {
-        $scope.myData.clearItem();
-    };
-
-    $scope.create = function () {
-        $scope.myData.createItem($scope.myData.currentItem);
-    };
-
 });
 
-app.controller('questions_controller', function (question_service, $stateParams, $state, $scope) {
+app.controller('edit_questions_controller', function (question_service, $stateParams, $state, $scope) {
+
+    $scope.mode = "edit";
 
     $scope.question_data = question_service;
     $scope.question_data.load_items();
@@ -68,36 +67,14 @@ app.controller('questions_controller', function (question_service, $stateParams,
         $state.go('questions');
     };
     
-    $scope.create_question = function () {
-        $scope.question_data.current_item = null;
-        $scope.question_data.create_items($scope.question_data.current_item);
+    $scope.delete_question = function () {
+        $scope.question_data.delete_items();
+        $scope.question_data.load_items();
+        $state.go('questions');
     };
 });
 
 // APP SERVICES
-
-app.service('q_a_service', function (q_a_resource) {
-    var self = {
-        'list': [],
-        'current_item': null,
-        'load_items': function () {
-            q_a_resource.get(function (response) {
-                angular.forEach(response.results, function (item) {
-                    console.log(item);
-                    self.list.push(new q_a_resource(item));
-                });
-            });
-        },
-        'update_items': function () {
-            self.current_item.$update();
-        },
-        'clear_items': function () {
-            self.current_item = null;
-        }
-    };
-
-    return self;
-});
 
 app.service('question_service', function (question_resource) {
     var self = {
@@ -125,9 +102,9 @@ app.service('question_service', function (question_resource) {
         },
         'create_items': function (question) {
             question_resource.save(question);
-            self.list = [];
-            self.current_item = null;
-            self.load_items();
+        },
+        'delete_items': function () {
+            self.current_item.$remove();
         }
     };
     return self;
